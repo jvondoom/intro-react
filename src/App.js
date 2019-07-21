dd5d267e10263e5f864c749df1bd37260b214aef
phazhion <phazhion@gmail.com>
2019-07-21 13:56:09 -0600

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Intro</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1>Intro</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
