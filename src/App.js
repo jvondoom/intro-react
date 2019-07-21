2e326f7b2983c223b1794db9007742d390d758d8
phazhion <phazhion@gmail.com>
2019-07-21 16:25:11 -0600

    Created Resume component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from './components/Header';
 import Influence from './components/Influence';
+import Resume from './components/Resume';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Influence />
+      <Resume />
     </div>
   );
 }
---
+++ src/components/Resume.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Resume = props => {
+  return (
+    <div className="resume">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h2 className="mb-4">Resume</h2>
+            <p>Now let's discuss the boring parts.</p>
+            <h3 className="mb-4 text-center">Working experience</h3>
+            <ul className="list-group mb-4 px-5">
+              <li className="list-group-item">
+                January 2019 - Present
+                <br />
+                Front End Developer; <b>Freelancer</b>
+              </li>
+              <li className="list-group-item">
+                June 2018 - January 2019
+                <br />
+                Front End Developer; <b>Havas Magma</b>
+              </li>
+              <li className="list-group-item">
+                August 2015 - June 2018
+                <br />
+                Front End Developer; <b>Hangar</b>
+              </li>
+            </ul>
+            <h3 className="mb-4 text-center">Skills</h3>
+            <p className="px-4">
+              <b>Tech skills:</b> ReactJS, MeteorJS, Bootstrap, SCSS, Webpack,
+              Docker.
+            </p>
+            <p className="px-4">
+              <b>Soft skills:</b> Analytic, proactive, fast learner, problem
+              solver.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Resume;
